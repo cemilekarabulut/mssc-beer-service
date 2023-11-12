0ab42faf1c0107e55a664cd383021a824c66d255
Cemile Karabulut <cemilekarablt@gmail.com>
2023-11-12 16:21:08 +0300

    inventory service exchange added

---
+++ src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@Disabled // utility for manual testing
+@SpringBootTest
+class BeerInventoryServiceRestTemplateImplTest {
+
+    @Autowired
+    BeerInventoryService beerInventoryService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getOnhandInventory() {
+        Integer qoh = beerInventoryService.getOnhandInventory(UUID.fromString("cd4be481-c933-46a7-a069-f09521948b9e"));
+        System.out.println(qoh);
+    }
+}
